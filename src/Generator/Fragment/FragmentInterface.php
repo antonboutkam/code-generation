413a4303d1e49f868a7dce2d27f41a002333c9cb
Anton Boutkam <anton@nui-boutkam.nl>
2021-01-05 11:05:19 +0100

    Removed unused code

--- src/Generator/Fragment/FragmentInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Generator\Fragment;
-
-use Core\DataType\PlainText;
-use Hurah\Types\Type\Path;
-
-interface FragmentInterface
-{
-}
--- src/Generator/Fragment/Php.php
+++
@@ -1,4 +0,0 @@
-<?php
-namespace Generator\Fragment;
-
-class Php extends Fragment implements FragmentInterface {}
